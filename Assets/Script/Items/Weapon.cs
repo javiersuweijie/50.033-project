a52a0ea86530d9f4312d0498153ddc6a256a24f3
Javier <javier.su.weijie@gmail.com>
2015-06-14 22:01:39 +0800

    add: decorator pattern for weapons and items

---
+++ Assets/Script/Items/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+enum Rarity {
+	Common,
+	Rare,
+	Epic,
+	Unique
+};
+public abstract class Weapon : Item {
+	int damage;
+	Rarity rarity;
+
+	void preAttack(Object attacker, Object target);
+	void calculateDamage(Object attacker, Object target);
+	void postAttack(Object attacker, Object target);
+}
